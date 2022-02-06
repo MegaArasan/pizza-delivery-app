e512247e3637f08b20ab71abe3a5dfca6d5812da
MegaArasan <megaarasan2000@gmail.com>
2022-02-06 21:55:53 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,183 +1,26 @@
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
 import "./App.css";
-import { Login } from "./components/Login.js";
+import { Login } from "./components/Authenication/Login.js";
 import Typography from "@mui/material/Typography";
-import { useHistory } from "react-router";
-import Button from "@mui/material/Button";
-import { IconButton } from "@mui/material";
-import { Switch, Route } from "react-router-dom";
-import { Signup } from "./components/Signup.js";
-import MenuIcon from "@mui/icons-material/Menu";
-import { Pizzalist } from "./components/Pizzalist.js";
-import { useState } from "react";
-import Drawer from "@mui/material/Drawer";
-import CloseIcon from "@mui/icons-material/Close";
-import TextField from "@mui/material/TextField";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import { Cart } from "./components/Cart.js";
+import { Appbar } from "./components/AppBar/Appbar.js";
+import Paper from "@mui/material/Paper";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { Signup } from "./components/Authenication/Signup.js";
+import { Forgotpassword } from "./components/Authenication/Forgotpassword.js";
+import { Resetpassword } from "./components/Authenication/Resetpassword.js";
+import { Pizzalist } from "./components/Homepage/Pizzalist.js";
+import { Cart } from "./components/Cart/Cart.js";
+import { useParams } from "react-router-dom";
+import CircularProgress from "@mui/material/CircularProgress";
+import { API_URL } from "./globalconstant.js";
+import { Settings } from "./components/settings/Settings.js";
+import { OrderHistory } from "./components/Cart/OrderHistory.js";
 
 export default function App() {
-  const [opened, setopened] = useState(false);
-  const handleDrawerOpen = () => {
-    setopened(true);
-  };
-  const handleDrawerClose = () => {
-    setopened(false);
-  };
-  const [open, setOpen] = useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const history = useHistory();
   return (
     <div className="App">
-      <AppBar
-        color="inherit"
-        sx={{ backgroundColor: "yellow" }}
-        position="static"
-      >
-        <Toolbar variant="dense">
-          <img
-            src="https://thumbs.dreamstime.com/z/homemade-pizza-bright-yellow-background-concept-traditional-italian-food-close-up-copy-space-152762617.jpg"
-            alt="pizza"
-            className="logo"
-          />
-          <Typography variant="h4" sx={{ fontFamily: "Pacifico" }}>
-            Mega's Pizza
-          </Typography>
-          <Button
-            sx={{ ml: "auto", display: { xs: "none", sm: "block" } }}
-            onClick={() => history.push("/")}
-            variant="text"
-          >
-            Home
-          </Button>
-          <Button
-            sx={{ display: { xs: "none", sm: "block" } }}
-            onClick={handleClickOpen}
-            variant="text"
-          >
-            Contact us
-          </Button>
-          <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Contact</DialogTitle>
-            <DialogContent>
-              <DialogContentText>
-                To tell us the query, please enter your email address and your
-                query here. We will send updates occasionally.
-              </DialogContentText>
-              <TextField
-                autoFocus
-                margin="dense"
-                id="name"
-                label="Email Address"
-                type="email"
-                fullWidth
-                variant="standard"
-              />
-              <TextField
-                autoFocus
-                margin="dense"
-                id="name"
-                multiline
-                label="Problem statement"
-                fullWidth
-                variant="standard"
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose}>Cancel</Button>
-              <Button onClick={handleClose}>Send Query</Button>
-            </DialogActions>
-          </Dialog>
-          <Button
-            sx={{ display: { xs: "none", sm: "block" } }}
-            onClick={() => history.push("/cart")}
-            variant="text"
-          >
-            cart
-          </Button>
-          <Button
-            sx={{ display: { xs: "none", sm: "block" } }}
-            onClick={() => history.push("/login")}
-            variant="text"
-          >
-            Login
-          </Button>
-          <Button
-            sx={{ display: { xs: "none", sm: "block" } }}
-            onClick={() => history.push("/signup")}
-            variant="text"
-          >
-            signup
-          </Button>
-          <IconButton
-            sx={{ display: { xs: "block", sm: "none" }, ml: "auto" }}
-            onClick={handleDrawerOpen}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Drawer
-            sx={{
-              width: "200px",
-              height: "100%",
-              flexShrink: 0,
-              "& .MuiDrawer-paper": {
-                width: "200px",
-                height: "100%",
-                boxSizing: "border-box",
-                backgroundColor: "#ffff00c4",
-                color: "black",
-                fontSize: "1em",
-                fontFamily: "Pacifico",
-              },
-            }}
-            anchor="right"
-            open={opened}
-          >
-            <IconButton
-              color="inherit"
-              aria-label="close"
-              component="span"
-              onClick={handleDrawerClose}
-              sx={{ ml: "auto" }}
-            >
-              <CloseIcon />
-            </IconButton>
-            <Button onClick={() => history.push("/")} variant="text">
-              Home
-            </Button>
-            <Button onClick={handleClickOpen} variant="text">
-              Contact us
-            </Button>
-            <Button onClick={() => history.push("/cart")} variant="text">
-              cart
-            </Button>
-            <Button onClick={() => history.push("/login")} variant="text">
-              Login
-            </Button>
-            <Button onClick={() => history.push("/signup")} variant="text">
-              signup
-            </Button>
-          </Drawer>
-        </Toolbar>
-      </AppBar>
       <Switch>
         <Route exact path="/">
-          <Pizzalist />
-        </Route>
-        <Route exact path="/cart">
-          <Cart />
+          <Redirect to="/login" />
         </Route>
         <Route exact path="/login">
           <Login />
@@ -185,7 +28,85 @@ export default function App() {
         <Route exact path="/signup">
           <Signup />
         </Route>
+        <Route exact path="/forgotpassword">
+          <Forgotpassword />
+        </Route>
+        <Route exact path="/forgotpassword/verify/:id">
+          <Changepass />
+        </Route>
+        <Route exact path="/resetpassword/:id">
+          <Resetpassword />
+        </Route>
+        <>
+          <Paper
+            elevation={0}
+            style={{ borderStyle: "none", minHeight: "100vh" }}
+          >
+            <Appbar />
+            <Route exact path="/settings">
+              <Settings />
+            </Route>
+            <Route exact path="/menu">
+              <Pizzalist />
+            </Route>
+            <Route exact path="/cart">
+              <Cart />
+            </Route>
+            <Route exact path="/orderhistory">
+              <OrderHistory />
+            </Route>
+          </Paper>
+        </>
       </Switch>
+    </div>
+  );
+}
+
+function Changepass() {
+  const { id } = useParams();
+  // console.log(id);
+  return id ? <Updatepassword id={id} /> : "";
+}
+// updatpassword
+function Updatepassword({ id }) {
+  // const { history } = useHistory();
+  // console.log(id);
+  const Result = (id) => {
+    fetch(`${API_URL}/users/forgotpassword/verify`, {
+      method: "GET",
+      headers: { "x-auth-token": id },
+    })
+      .then((response) => {
+        const Status = response.status;
+        // console.log(Status);
+        return Status;
+      })
+      .then((Status) =>
+        Status === 200
+          ? window.location.replace(`/resetpassword/${id}`)
+          : alert("Please enter the registered email")
+      );
+  };
+
+  Result(id);
+
+  // Loading Page
+  return (
+    <div
+      className="loader-container"
+      style={{
+        display: "flex",
+        height: "100vh",
+        flexDirection: "column",
+        alignContent: "center",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <CircularProgress color="success" />
+      <Typography sx={{ fontFamily: "Aladin" }} variant="h6">
+        Please Wait......
+      </Typography>
     </div>
   );
 }
--- src/components/Homepage/Pizzalist.js
+++ src/components/Homepage/Pizzalist.js
@@ -2,22 +2,27 @@ import { Pizza } from "./Pizza.js";
 import { useEffect, useState } from "react";
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
+import { useHistory } from "react-router-dom";
+import { API_URL } from "../../globalconstant.js";
 
 export function Pizzalist() {
+  const history = useHistory();
   const [pizzas, setpizzas] = useState([]);
-  const API_URL = "https://pizza-delivary-app.herokuapp.com/pizzas";
   useEffect(() => {
-    fetch(`${API_URL}`)
+    fetch(`${API_URL}/pizzas`)
       .then((data) => data.json())
       .then((pizzas) => setpizzas(pizzas));
   }, []);
-
+  const Username = localStorage.getItem("Username");
+  if (!Username) {
+    history.push("/");
+  }
   return (
     <div className="pizzas-list">
       {pizzas && pizzas.length < 0 ? (
         <Loader />
       ) : (
-        pizzas.map((pizza,index) => <Pizza pizza={pizza}key={index} />)
+        pizzas.map((pizza, index) => <Pizza pizza={pizza} key={index} />)
       )}
       {/* {pizzas.map((pizza) => (
         <Pizza pizza={pizza} />
@@ -25,7 +30,7 @@ export function Pizzalist() {
     </div>
   );
 }
-function Loader() {
+export function Loader() {
   const [open] = useState(true);
   return (
     <div className="loading">
